38cf53c5d719eac4b9c756a31a8f9cef837dfefc
Martin Rafael Gonzalez <tin@devtin.io>
2019-05-23 17:16:17 -0400

    remove dummy project

--- test/fixtures/project/api/provider.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  discriminator: 'user',
-  model: {
-    schema: {
-      companyName: String
-    }
-  }
-}
--- test/fixtures/project/pleasure.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  api: {
-    debug: process.env.NODE_ENV === 'development',
-    entitiesUri: '/schemas'
-  }
-}

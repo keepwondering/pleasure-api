8d248435a3d543b0c9a3e816b6ffb67384dac191
Martin Rafael Gonzalez <tin@devtin.io>
2019-05-23 17:18:41 -0400

    fix abstracting dummy project

--- test/utils/web-server.js
+++ test/utils/web-server.js
@@ -1,4 +1,4 @@
 const emulate = require('pleasure-core-dev-tools/test/emulate-server.js')
 const path = require("path")
 
-emulate(path.join(__dirname, '../fixtures/project'))
+emulate(path.join(__dirname, '../fixtures/pleasure-dummy-project'))

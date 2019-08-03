ecfcdd520fd3a2838ab99e13d8f61fb8f53934a0
Martin Rafael Gonzalez <tin@devtin.io>
2019-08-03 15:05:20 -0400

    add helmet plugin name

--- src/lib/plugins/helmet.js
+++ src/lib/plugins/helmet.js
@@ -2,6 +2,7 @@ import helmet from 'koa-helmet'
 import { getConfig } from 'pleasure-api'
 
 export default {
+  name: 'helmet',
   prepare ({ router }) {
     const { helmet: helmetConfig = {} } = getConfig('api')
     router.use(helmet(helmetConfig))
